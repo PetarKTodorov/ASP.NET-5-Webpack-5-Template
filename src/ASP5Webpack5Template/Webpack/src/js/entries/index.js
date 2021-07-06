43e572cc296976552f2b6c7fae8be9eb97680748
PetarKTodorov <PetarKTodorov@gmail.com>
2021-07-06 21:34:48 +0300

    add basic webpack config files

---
+++ src/ASP5Webpack5Template/Webpack/src/js/entries/index.js
@@ -0,0 +1,15 @@
+﻿const user = {
+    "name": "Adam",
+    "age": 20,
+    "gender": "Male",
+    "country": "BG",
+};
+
+const user1 = {
+    ...user,
+    name: "Eva",
+    "gender": "Female",
+}
+
+console.log(user);
+console.log(user1);
