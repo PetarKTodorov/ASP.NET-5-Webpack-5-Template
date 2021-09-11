7371100f16326aa12029fd23f2ebf75b52d13704
PetarKTodorov <PetarKTodorov@gmail.com>
2021-09-11 12:46:34 +0300

    Remove test files

--- src/WebApplication/Webpack/src/js/entries/index.js
+++ src/WebApplication/Webpack/src/js/entries/index.js
@@ -4,18 +4,3 @@ import 'jquery-validation-unobtrusive';
 
 import 'bootstrap';
 
-const user = {
-    "name": "Adam",
-    "age": 20,
-    "gender": "Male",
-    "country": "BG",
-};
-
-const user1 = {
-    ...user,
-    name: "Eva",
-    "gender": "Female",
-}
-
-console.log(user);
-console.log(user1);
